313b9c5c110ea3830bf1b010e1bcf0e68d92d2e5
Trimutex <gregcraw2001@gmail.com>
2024-04-23 11:22:57 -0500

    extraction: clean additional files and reduce iterations

--- extract.sh
+++ extract.sh
@@ -1,18 +1,15 @@
 mkdir /tmp/extract
 for f in *.bed
 do
-    bedtools getfasta -bed "$f" -fi "/lfs/hg38.fa" -fo /tmp/extract/"$f"p.txt
-    bedtools complement -i "$f" -g /lfs/hg38.fa.fai > /tmp/extract/"$f"n 
-    bedtools getfasta -bed /tmp/extract/"$f"n -fi "/lfs/hg38.fa" -fo /tmp/extract/"$f"n.txt
+    bedtools getfasta -bed "$f" -fi "/lfs/hg38.fa" -fo /tmp/extract/"$f"p
+    bedtools complement -i "$f" -g /lfs/hg38.fa.fai > /tmp/extract/"$f"c 
+    bedtools getfasta -bed /tmp/extract/"$f"c -fi "/lfs/hg38.fa" -fo /tmp/extract/"$f"n
+    echo "Finished with a complement file, cleaning..."
+    rm /tmp/extract/"$f"c -f
+    grep -v '>' /tmp/extract/"$f"p | cut -c 1-120 - | awk 'length($0)==120' > /tmp/extract/"$f".dnap;
+    echo "Finished with a positive file, cleaning..."
+    rm /tmp/extract/"$f"p -f
+    grep -v '>' /tmp/extract/"$f"n | cut -c 1-120 - | awk 'length($0)==120' > /tmp/extract/"$f".dnan;
+    echo "Finished with a negative file, cleaning..."
     rm /tmp/extract/"$f"n -f
-done
-
-for f in /tmp/extract/*p.txt
-do
-    grep -v '>' "$f" | cut -c 1-120 - | awk 'length($0)==120' > "$f".dnap;
-done
-
-for f in /tmp/extract/*n.txt
-do
-    grep -v '>' "$f" | cut -c 1-120 - | awk 'length($0)==120' > "$f".dnan;
 done
