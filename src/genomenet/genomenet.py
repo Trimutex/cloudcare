082e25d6083f83853d59867adfee828531dd45b1
Trimutex <gregcraw2001@gmail.com>
2024-04-25 11:39:15 -0500

    model: change model to AlexNet

--- src/genomenet/genomenet.py
+++ src/genomenet/genomenet.py
@@ -128,11 +128,56 @@ class GenomeSet(Dataset):
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.linear1 = nn.Linear(4, 16)
-        self.linear2 = nn.Linear(16, CLASSES)
-        self.relu = F.relu
+        self.conv01 = nn.Conv2d(in_channels=4, out_channels=96,
+                                kernel_size=11, stride=4, padding=0)
+        self.conv02 = nn.Conv2d(in_channels=96, out_channels=96,
+                                kernel_size=1)
+        self.conv03 = nn.Conv2d(in_channels=96, out_channels=96,
+                                kernel_size=1)
+        self.pool03 = nn.MaxPool2d(kernel_size=3, stride=2)
+        self.conv04 = nn.Conv2d(in_channels=96, out_channels=256,
+                                kernel_size=11, stride=4, padding=2)
+        self.conv05 = nn.Conv2d(in_channels=256, out_channels=256,
+                                kernel_size=1)
+        self.pool05 = nn.MaxPool2d(kernel_size=3, stride=2)
+        self.conv06 = nn.Conv2d(in_channels=256, out_channels=384,
+                                kernel_size=3, stride=1, padding=1)
+        self.conv07 = nn.Conv2d(in_channels=384, out_channels=384,
+                                kernel_size=1)
+        self.conv08 = nn.Conv2d(in_channels=384, out_channels=384,
+                                kernel_size=1)
+        self.conv09 = nn.Conv2d(in_channels=384, out_channels=10,
+                                kernel_size=3, stride=1, padding=1)
+        self.conv10 = nn.Conv2d(in_channels=10, out_channels=10,
+                                kernel_size=1)
+        self.conv11 = nn.Conv2d(in_channels=10, out_channels=CLASSES,
+                                kernel_size=1)
 
     def forward(self, x):
-        x = self.relu(self.linear1(x))
-        x = self.relu(self.linear2(x))
+        # Layer 01
+        x = F.relu(self.conv01(x))
+        # Layer 02
+        x = F.relu(self.conv02(x))
+        # Layer 03
+        x = self.pool03(F.relu(self.conv03(x)))
+        x = F.dropout(x, 0.5)
+        # Layer 04
+        x = F.relu(self.conv04(x))
+        # Layer 05
+        x = self.pool05(F.relu(self.conv05(x)))
+        x = F.dropout(x, 0.5)
+        # Layer 06
+        x = F.relu(self.conv06(x))
+        # Layer 07
+        x = F.relu(self.conv07(x))
+        # Layer 08
+        x = F.relu(self.conv08(x))
+        x = F.dropout(x, 0.5)
+        # Layer 09
+        x = F.relu(self.conv09(x))
+        # Layer 10
+        x = F.relu(self.conv10(x))
+        # Layer 11
+        x = F.relu(self.conv11(x))
+        x = nn.AdaptiveAvgPool2d((1, 1))(x)
         return x
