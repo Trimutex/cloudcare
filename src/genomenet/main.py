6954cf3fb825c17f7a35978f7f1486c006b4d745
Trianta <56975502+Trimutex@users.noreply.github.com>
2024-04-24 21:14:10 -0500

    info: print info before loading instead

--- src/genomenet/main.py
+++ src/genomenet/main.py
@@ -6,8 +6,8 @@ if __name__ == '__main__':
     seed = 42
     EPOCHS = 2
     network = genomenet.GenomeNet(seed, EPOCHS)
-    network.load("../../data")
     network.info()
+    network.load("../../data")
     for current_epoch in range(1, network.epochs + 1):
         network.train(current_epoch)
         network.test()
