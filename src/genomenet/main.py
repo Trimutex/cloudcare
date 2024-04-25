ba2b83c5267408ff9a4f8827f5c1bac80ade7eb1
Trianta <56975502+Trimutex@users.noreply.github.com>
2024-04-24 20:20:10 -0500

    genomenet: python skill issue

--- src/genomenet/main.py
+++ src/genomenet/main.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-import genomenet as GenomeNet
+import genomenet
 
 if __name__ == '__main__':
     seed = 42
     EPOCHS = 2
-    network = GenomeNet(seed, EPOCHS)
+    network = genomenet.GenomeNet(seed, EPOCHS)
     network.load("../../data")
     for current_epoch in range(1, network.epochs + 1):
         network.train(current_epoch)
