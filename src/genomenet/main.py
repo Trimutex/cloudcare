271326ceeb835d3a4b339e7828381dbe614fd0e6
Trianta <56975502+Trimutex@users.noreply.github.com>
2024-04-24 20:15:58 -0500

    genomenet: minor import name change

--- src/genomenet/main.py
+++ src/genomenet/main.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-import genomenet
+import genomenet as GenomeNet
 
 if __name__ == '__main__':
     seed = 42
     EPOCHS = 2
-    network = genomenet(seed, EPOCHS)
+    network = GenomeNet(seed, EPOCHS)
     network.load("../../data")
     for current_epoch in range(1, network.epochs + 1):
         network.train(current_epoch)
